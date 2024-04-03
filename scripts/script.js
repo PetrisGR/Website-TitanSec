846e18a0919f984e253e31595da7588b6583ebeb
Stamatis Petris <121623120+PetrisGR@users.noreply.github.com>
2024-04-03 03:19:06 +0300

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+function GoBackHome() {
+    window.location.href = "/";
+}
+
+function ImInterestedInServiceBtn(service) {
+    window.location.href = "/pages/contact.html?subject=" + "Interested in " + service + " Service" + "&message=" + "I am interested in the " + service + " service. Please contact me with more information.";
+}
+
+function ApplyNowBtn() {
+    const sectionElement = document.getElementById('job_application_form');
+    sectionElement.scrollIntoView({behavior: "smooth"});
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var menuIcon = document.getElementById("menuIcon");
+    var navLinks = document.getElementById("navLinks");
+    
+    // Responsive Menu Icon for Small Dimension Screens (Mobile)
+    menuIcon.addEventListener("click", function() {
+        if (navLinks.classList.contains("active")) {
+            navLinks.classList.remove("active");
+        } else {
+            navLinks.classList.add("active");
+        }
+    });
+
+    // Get URL Query Parameters
+    const urlParams = new URLSearchParams(window.location.search);
+        
+    // Fill in the subject and message fields with the URL query parameters
+    const subject = urlParams.get('subject');
+    const message = urlParams.get('message');
+
+    if (subject === null || message === null) {
+        return;
+    }
+
+    document.getElementById('subject').value = subject;
+    document.getElementById('message').value = message;
+});
